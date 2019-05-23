7adb9cdf9964babe69fdcad7b9becd1ab6891227
Iris Kim <kimi4149@sduhsd.net>
2019-05-23 18:51:17 +0000

    divide

--- cdemo/ifstmnt.c
+++ cdemo/ifstmnt.c
@@ -15,10 +15,40 @@ int main()
    // if statement to test if a is not equal to 0
    if (a != 0)
    {
-     printf("a is equal to 0\n");
+     printf("a is not equal to 0\n");
    }
    else
    {
      printf("a is equal to 0\n");
+   }
+   int a > 0
+   // if statement to test if a is greater than b
+   if (a > 0)
+   {
+     printf("a is greater than 0\n");
+   }
+   else
+   {
+     printf("a is less than 0\n");
+   }
+   int (a >= 0)
+   // if statement to test is a is greater than or equal to 0
+   if (a >= 0)
+   {
+     printf("a is greater than or equal to 0\n");
+   }
+   else
+   {
+     printf("a is less than 0\n");
+   }
+   int (a == 0 && b == 0)
+   // if statement to test is a is equal to 0 and b is equal to 0
+   if (a == 0 && b == 0)
+   {
+     printf("a is equal to 0 and b is equal to 0\n");
+   }
+   else
+   {
+     printf("a is not equal to 0 and b is not equal to zero");
    }
 }
