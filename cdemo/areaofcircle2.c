078e9b071e07d46f305f23b184667dd7be4124a9
kimi4149 <50209711+kimi4149@users.noreply.github.com>
2019-05-31 11:46:18 -0700

    areaofcircle2.c

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -1,68 +1,41 @@
 #include <stdio.h>
-#include <math.h>
 
-  float areaofcircle(float r)
+float areaOfCircle(float radius) 
 {
-  float area = 3.14 * r * r;
-  return area; 
+  float area = (3.14159265358979323846 * radius * radius);
+  return area;
 }
 
-  float radiusminimum()
+int main(int argc, char* argv[])
 {
-    float radiusmin;
-    char input [256];
-    int correct = 0;
-
-  while (correct == 0)
-    {
-    printf("What is the minimum radius of the circle? \n");
-    fgets(input, 256, stdin);
-    correct = sscanf(input, "%f", &radiusmin);
-    }
-   
-  if (correct != 1)
-    {
-    printf("Enter a number\n");
-    }
-  return radiusmin;
-}
-
-float radiusmaximum()
-{
-    float radiusmax;
-    char input2 [256];
-    int correct2 = 0;
-
-  while (correct2 == 0)
-    {
-    printf("What is the maximum radius of the circle? \n");
-    fgets(input2, 256, stdin);
-    correct2 =  sscanf(input2, "%f", &radiusmax);
-    }
-   
-  if (correct2 != 1)
-    {
-    printf("Enter a number\n"); 
-    }
-  return radiusmax;
-}
-
- int main()
-{
-  float rmin = radiusminimum();
-  float rmax = radiusmaximum();
-  
-  if (rmax <= rmin)
-{
-  printf("The value of the minimum radius must be less than the value of the maximum radius.\n");
-  printf("What is the minimum radius of the circle?\n");
-  scanf("%f", &rmax);
-}
-
-  for (float i = rmin; i <=rmax; i++)
+ if (argc != 3)
   {
-    float a = areaofcircle(i);
-    printf("area = %f\n", a); 
+    printf("%s : expected 2 args, please enter two floats\n", argv[0]);
+    return 1;
   }
+  
+  float lower;
+  int found = sscanf(argv[1], "%f", &lower);
+  if (found != 1)
+    {
+	printf("First arg is not a decimal, enter two floats\n");
+	return 1;
+    }
 
+  float upper;
+  found = sscanf(argv[2], "%f", &upper);
+  if (found != 1)
+    {
+	printf("Second arg is not a decimal, enter two floats\n");
+	return 1;
+    }
+  //printf("Enter a lower bound:\n");
+  //scanf("%f", &lower);
+  //printf("Enter an upper bound:\n");
+  //scanf("%f", &upper);
+  for (float radius = lower; radius <= upper; radius++)
+    {
+	float result = areaOfCircle(radius);
+	printf("Area at r = %f: %f\n", radius, result);
+    }
 }
